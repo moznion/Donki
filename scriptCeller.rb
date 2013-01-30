6449d9b0f306c6308c69fe270af5f63f3116d7b0
moznion <moznion@gmail.com>
2013-01-30 17:36:07 +0900

    Implement function of reading profiles.

--- scriptCeller.rb
+++ scriptCeller.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
+require 'json'
 require 'grit'
 include Grit
 
 module Celler
-
   class Git
     def initialize(git_repos, temp_dir='./')
       @repos    = git_repos
@@ -16,7 +16,7 @@ module Celler
       #   git://github.com/foo/bar.git
       #                        ~~~
       #                         |-- extract
-      @dir = @repos.split('/')[-1].split('.')[0];
+      @dir = @repos.split('/')[-1].split('.')[0]
     end
 
     def clone
@@ -31,8 +31,33 @@ module Celler
     end
   end
 
+  # TODO correspond to YAML?
+  # Now, this class can handle JSON only.
+  class Configure
+    def initialize(filename)
+      @config_file = filename
+    end
+
+    # TODO @config should be member?
+    def readConfigFile
+      File.open(@config_file, :encoding => Encoding::UTF_8) do |file|
+        @config = file.read
+      end
+    end
+    private :open
+
+    def parse
+      readConfigFile
+      JSON.parse(@config)
+    end
+  end
 end
 
-# clone repos
-git = Celler::Git.new('git://github.com/moznion/vimrc.git')
-git.clone
+config = Celler::Configure.new('./.script_cellar_profile') # FIXME rc file name and location
+profiles = config.parse
+
+repositories = profiles['repositories']
+repositories.each do |repos|
+  git = Celler::Git.new(repos)
+  git.clone
+end
